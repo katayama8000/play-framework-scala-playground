f72a4525c8ed6a7aa546d424a8726aae43297e3b
katayama <tattu.0310@gmail.com>
2022-05-30 02:15:46 +0900

    ✨ マジやぞ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { useForm } from "@mantine/form";
 import { TextInput, Button, Box } from "@mantine/core";
 import { useCallback } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDoc, doc } from "firebase/firestore";
 import { db } from "@config/firebase";
 import { showNotification } from "@mantine/notifications";
 
@@ -13,9 +13,20 @@ const Home: NextPage = () => {
     },
   });
 
+  const create = async () => {
+    const docRef = doc(db, "todos", "xGiaP2IiAG6jd1AwX3tJ");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
   const handleSubmit = useCallback(async (values: { todo: string }) => {
     console.log("values", values);
-
     try {
       await addDoc(collection(db, "todos"), {
         expire: Date.now() + 1000 * 60 * 60 * 24,
@@ -40,6 +51,7 @@ const Home: NextPage = () => {
   return (
     <div className="p-20">
       <h1>Hello Next.js 👋</h1>
+      <Button onClick={create}>マジでとる</Button>
       <Box sx={{ maxWidth: 300 }} mx="auto">
         <form
           onSubmit={form.onSubmit((values) => handleSubmit(values))}
