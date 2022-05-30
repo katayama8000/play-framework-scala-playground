9865473fbfda652842dbfefd864c25712695ab17
katayama <tattu.0310@gmail.com>
2022-05-30 18:52:33 +0900

    ⚡️

--- src/components/layout/YouTube.tsx
+++ src/components/layout/YouTube.tsx
@@ -7,12 +7,13 @@ export const YouTube: React.FC<Props> = ({ link }) => {
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
+        width="336"
+        height="189"
         src={link}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        className="p-2"
       />
     </div>
   );
--- src/pages/linkform.tsx
+++ src/pages/linkform.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import Link from "next/link";
 import { config } from "@config/supabase/supabase";
-import { TextInput, Button, Group, Box } from "@mantine/core";
+import { TextInput, Button, Group, Box, Grid } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
+import { Chips, Chip } from "@mantine/core";
 import { YouTube } from "@components/layout/YouTube";
 
 type links = {
@@ -41,21 +42,20 @@ const linkform: NextPage = () => {
     link2: string;
     link3: string;
   }) => {
-    try {
-      const { data, error } = await config.supabase
-        .from("Links")
-        .insert([
-          { link1: values.link1, link2: values.link2, link3: values.link3 },
-        ]);
-      if (error) {
-        makeNotification("失敗", "再度入力してください", "red");
-      }
-      if (data) {
-        makeNotification("成功", "これ見てやる気を出せ！", "indigo");
-      }
-    } catch (e) {
+    const { data, error } = await config.supabase.from("Links").upsert([
+      {
+        id: 0,
+        link1: values.link1,
+        link2: values.link2,
+        link3: values.link3,
+      },
+    ]);
+    if (data) {
+      makeNotification("成功", "これ見てやる気を出せ！", "indigo");
+    } else if (error) {
       makeNotification("失敗", "再度入力してください", "red");
     }
+    form.reset();
   };
 
   useEffect(() => {
@@ -65,14 +65,14 @@ const linkform: NextPage = () => {
         makeNotification("失敗", "再度試してください", "red");
       }
       if (data) {
-        const l1 = makeLink(data[2].link1);
-        const l2 = makeLink(data[2].link2);
-        const l3 = makeLink(data[2].link3);
+        const l1 = makeLink(data[0].link1);
+        const l2 = makeLink(data[0].link2);
+        const l3 = makeLink(data[0].link3);
         setLinks({ link1: l1, link2: l2, link3: l3 });
       }
     };
     get();
-  }, []);
+  }, [links]);
 
   return (
     <div>
@@ -103,9 +103,11 @@ const linkform: NextPage = () => {
           </Group>
         </form>
       </Box>
-      <YouTube link={links?.link1} />
-      <YouTube link={links?.link2} />
-      <YouTube link={links?.link3} />
+      <div className="flex-center flex pt-4">
+        <YouTube link={links?.link1} />
+        <YouTube link={links?.link2} />
+        <YouTube link={links?.link3} />
+      </div>
     </div>
   );
 };
