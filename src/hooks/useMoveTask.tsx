f88b73a7092ca6a620181ebf9340d5d2e1077122
katayama <tattu.0310@gmail.com>
2022-06-05 12:24:42 +0900

    ⚡️

--- src/hooks/useMoveTask.tsx
+++ src/hooks/useMoveTask.tsx
@@ -1,15 +1,21 @@
 import { config } from "@config/supabase/supabase";
 
 export const useMoveTask = async () => {
-  //   const { data, error } = await config.supabase.from("TomorrowToDos").select();
-  //   console.log("useMoveTask", data![0]?.todo);
+  const { data, error } = await config.supabase.from("TomorrowToDos").select();
 
-  //   await config.supabase.from("ToDos").insert([
-  //     {
-  //       todo: data![0]?.todo,
-  //       isFinished: false,
-  //     },
-  //   ]);
+  for (let i = 0; i < data!.length; i++) {
+    await config.supabase.from("ToDos").insert([
+      {
+        todo: data![i].todo,
+        isFinished: false,
+      },
+    ]);
+  }
 
-  await config.supabase.from("TomorrowToDos").delete().match({ id: 14 });
+  for (let i = 0; i < data!.length; i++) {
+    await config.supabase
+      .from("TomorrowToDos")
+      .delete()
+      .match({ id: data![i].id });
+  }
 };
